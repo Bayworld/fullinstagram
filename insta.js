284ae38bcd89e0a3d0aa83cf27746e0393064381
Bayworld <abayomikiel@gmail.com>
2020-12-08 22:08:57 +0100

    instagram

--- insta.js
+++ insta.js
@@ -2,4 +2,6 @@ document.getElementById("follow").addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("follow").innerHTML = "unfollow";
+  document.getElementById("unfollow").innerHTML = "follow";
 }
+
